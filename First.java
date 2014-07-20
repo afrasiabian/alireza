73721927ebb7b1f7ac618b13a8d44677d3476b9d
Alireza <alireza.afrasiabian@gmail.com>
2014-07-20 08:27:51 -0400

    My Changes related to Feature 1

--- First.java
+++ First.java
@@ -3,3 +3,6 @@ This is first class.
 Lat Edit.
 	
 a new change.
+	
+	
+Feature 1 change.
--- Second.java
+++
@@ -1,6 +0,0 @@
-This is second class.
-
-Edited
-
-a new change in second file.
-a very new change in second file.
